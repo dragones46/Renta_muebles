b94da0bfce1289a9bd0276d01cd2bf77443e0447
dragones46 <deivy273@gmail.com>
2025-03-19 09:37:01 -0500

    cambie el lugar de los archivos

--- muebles/static/muebles/js/deshabilitar.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    document.addEventListener('contextmenu', function(e) {
-        e.preventDefault(); // Previene el menú contextual por defecto
-    });
-});
